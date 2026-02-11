1808ba7807f68bd2e10fb133a016bd938ae56052
Stan <stanbromley16@gmail.com>
2026-02-11 17:42:45 +0000

    converted turn to a more basic type

---
+++ src/Core/Domain/Types/Turn.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Domain.Types;
+
+/// <summary>
+/// Represents the current turn index and active team.
+/// Immutable value stored inside game state.
+/// </summary>
+public readonly struct Turn
+{
+    public int TurnNumber { get; }
+    public TeamId TeamToAct { get; }
+
+    public Turn(int turnNumber, TeamId teamToAct)
+    {
+        TurnNumber = turnNumber;
+        TeamToAct = teamToAct;
+    }
+
+    public override string ToString() => $"Turn {TurnNumber} ({TeamToAct})";
+}
