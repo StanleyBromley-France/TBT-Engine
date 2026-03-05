b0d517958b2b958779255ea669ffd932ac86cfb5
Stan <stanbromley16@gmail.com>
2026-03-05 15:48:48 +0000

    added action validator and generator tests

---
+++ tests/Core.Tests/Engine/Rules/ActionGeneratorTests.cs
@@ -0,0 +1,72 @@
+using Core.Domain.Abilities;
+using Core.Domain.Abilities.Targeting;
+using Core.Domain.Repositories;
+using Core.Domain.Types;
+using Core.Engine.Actions.Choice;
+using Core.Engine.Rules;
+using Core.Game;
+using Core.Map.Grid;
+using Core.Map.Pathfinding;
+using Core.Tests.Engine.TestSupport;
+
+namespace Core.Tests.Engine.Rules;
+
+public class ActionGeneratorTests
+{
+    [Fact]
+    public void GetLegalActions_Generates_AbilityActions_For_Valid_Enemy_Targets()
+    {
+        var ability = EngineTestFactory.CreateAbility("test", manaCost: 2, targetType: TargetType.Enemy, range: 5);
+        var caster = EngineTestFactory.CreateUnit(1, 1, new HexCoord(0, 0), mana: 10, abilityIds: ability.Id);
+        var ally = EngineTestFactory.CreateUnit(2, 1, new HexCoord(1, 0));
+        var enemy = EngineTestFactory.CreateUnit(3, 2, new HexCoord(1, -1));
+        var state = EngineTestFactory.CreateState(new[] { caster, ally, enemy }, teamToAct: 1, activeUnitId: caster.Id);
+
+        var generator = new ActionGenerator(
+            new StubPathfinder(),
+            new AbilityRepository(new[] { new KeyValuePair<AbilityId, Ability>(ability.Id, ability) }),
+            new AllowAllValidator());
+
+        var useActions = generator.GetLegalActions(state).OfType<UseAbilityAction>().ToList();
+
+        Assert.Single(useActions);
+        Assert.Equal(caster.Id, useActions[0].UnitId);
+        Assert.Equal(ability.Id, useActions[0].AbilityId);
+        Assert.Equal(enemy.Id, useActions[0].Target);
+    }
+
+    [Fact]
+    public void GetLegalActions_DoesNot_Generate_AbilityActions_When_Mana_Is_Insufficient()
+    {
+        var ability = EngineTestFactory.CreateAbility("test", manaCost: 50, targetType: TargetType.Enemy, range: 5);
+        var caster = EngineTestFactory.CreateUnit(1, 1, new HexCoord(0, 0), mana: 10, abilityIds: ability.Id);
+        var enemy = EngineTestFactory.CreateUnit(2, 2, new HexCoord(1, 0));
+        var state = EngineTestFactory.CreateState(new[] { caster, enemy }, teamToAct: 1, activeUnitId: caster.Id);
+
+        var generator = new ActionGenerator(
+            new StubPathfinder(),
+            new AbilityRepository(new[] { new KeyValuePair<AbilityId, Ability>(ability.Id, ability) }),
+            new AllowAllValidator());
+
+        var useActions = generator.GetLegalActions(state).OfType<UseAbilityAction>().ToList();
+
+        Assert.Empty(useActions);
+    }
+
+    private sealed class StubPathfinder : IPathfinder
+    {
+        public IReadOnlyDictionary<HexCoord, int> GetReachable(IReadOnlyMap map, HexCoord start, int maxMoves) =>
+            new Dictionary<HexCoord, int> { [start] = 0 };
+
+        public bool IsMoveValid(IReadOnlyMap map, HexCoord start, HexCoord destination, int maxMoves) => true;
+
+        public int? GetMoveCost(IReadOnlyMap map, HexCoord start, HexCoord destination) => 1;
+
+        public bool HasLineOfSight(IReadOnlyMap map, HexCoord from, HexCoord to) => true;
+    }
+
+    private sealed class AllowAllValidator : IActionValidator
+    {
+        public bool IsActionLegal(IReadOnlyGameState state, ActionChoice action) => true;
+    }
+}
---
+++ tests/Core.Tests/Engine/Rules/ActionValidatorTests.cs
@@ -0,0 +1,97 @@
+using Core.Domain.Abilities;
+using Core.Domain.Abilities.Targeting;
+using Core.Domain.Repositories;
+using Core.Domain.Types;
+using Core.Engine.Actions.Choice;
+using Core.Engine.Rules;
+using Core.Game;
+using Core.Map.Grid;
+using Core.Map.Pathfinding;
+using Core.Tests.Engine.TestSupport;
+
+namespace Core.Tests.Engine.Rules;
+
+public class ActionValidatorTests
+{
+    [Fact]
+    public void UseAbility_IsIllegal_When_Target_Is_Out_Of_Range()
+    {
+        var ability = EngineTestFactory.CreateAbility("shot", manaCost: 1, targetType: TargetType.Enemy, range: 1);
+        var caster = EngineTestFactory.CreateUnit(1, 1, new HexCoord(0, 0), mana: 10, abilityIds: ability.Id);
+        var target = EngineTestFactory.CreateUnit(2, 2, new HexCoord(3, 0));
+        var state = EngineTestFactory.CreateState(new[] { caster, target }, teamToAct: 1, activeUnitId: caster.Id);
+
+        var validator = new ActionValidator(
+            new StubPathfinder { HasLineOfSightResult = true },
+            new AbilityRepository(new[] { new KeyValuePair<AbilityId, Ability>(ability.Id, ability) }));
+
+        var legal = validator.IsActionLegal(state, new UseAbilityAction(caster.Id, ability.Id, target.Id));
+
+        Assert.False(legal);
+    }
+
+    [Fact]
+    public void UseAbility_IsLegal_When_Target_Is_In_Range_And_Valid()
+    {
+        var ability = EngineTestFactory.CreateAbility("stab", manaCost: 1, targetType: TargetType.Enemy, range: 2);
+        var caster = EngineTestFactory.CreateUnit(1, 1, new HexCoord(0, 0), mana: 10, abilityIds: ability.Id);
+        var target = EngineTestFactory.CreateUnit(2, 2, new HexCoord(1, 0));
+        var state = EngineTestFactory.CreateState(new[] { caster, target }, teamToAct: 1, activeUnitId: caster.Id);
+
+        var validator = new ActionValidator(
+            new StubPathfinder { HasLineOfSightResult = true },
+            new AbilityRepository(new[] { new KeyValuePair<AbilityId, Ability>(ability.Id, ability) }));
+
+        var legal = validator.IsActionLegal(state, new UseAbilityAction(caster.Id, ability.Id, target.Id));
+
+        Assert.True(legal);
+    }
+
+    [Fact]
+    public void UseAbility_IsIllegal_When_LineOfSight_Required_But_Blocked()
+    {
+        var ability = EngineTestFactory.CreateAbility("beam", manaCost: 1, targetType: TargetType.Enemy, range: 3, requiresLos: true);
+        var caster = EngineTestFactory.CreateUnit(1, 1, new HexCoord(0, 0), mana: 10, abilityIds: ability.Id);
+        var target = EngineTestFactory.CreateUnit(2, 2, new HexCoord(1, 0));
+        var state = EngineTestFactory.CreateState(new[] { caster, target }, teamToAct: 1, activeUnitId: caster.Id);
+
+        var validator = new ActionValidator(
+            new StubPathfinder { HasLineOfSightResult = false },
+            new AbilityRepository(new[] { new KeyValuePair<AbilityId, Ability>(ability.Id, ability) }));
+
+        var legal = validator.IsActionLegal(state, new UseAbilityAction(caster.Id, ability.Id, target.Id));
+
+        Assert.False(legal);
+    }
+
+    [Fact]
+    public void UseAbility_IsIllegal_When_Target_Type_Does_Not_Match()
+    {
+        var ability = EngineTestFactory.CreateAbility("enemy-only", manaCost: 1, targetType: TargetType.Enemy, range: 3);
+        var caster = EngineTestFactory.CreateUnit(1, 1, new HexCoord(0, 0), mana: 10, abilityIds: ability.Id);
+        var ally = EngineTestFactory.CreateUnit(2, 1, new HexCoord(1, 0));
+        var state = EngineTestFactory.CreateState(new[] { caster, ally }, teamToAct: 1, activeUnitId: caster.Id);
+
+        var validator = new ActionValidator(
+            new StubPathfinder { HasLineOfSightResult = true },
+            new AbilityRepository(new[] { new KeyValuePair<AbilityId, Ability>(ability.Id, ability) }));
+
+        var legal = validator.IsActionLegal(state, new UseAbilityAction(caster.Id, ability.Id, ally.Id));
+
+        Assert.False(legal);
+    }
+
+    private sealed class StubPathfinder : IPathfinder
+    {
+        public bool HasLineOfSightResult { get; init; } = true;
+
+        public IReadOnlyDictionary<HexCoord, int> GetReachable(IReadOnlyMap map, HexCoord start, int maxMoves) =>
+            new Dictionary<HexCoord, int> { [start] = 0 };
+
+        public bool IsMoveValid(IReadOnlyMap map, HexCoord start, HexCoord destination, int maxMoves) => true;
+
+        public int? GetMoveCost(IReadOnlyMap map, HexCoord start, HexCoord destination) => 1;
+
+        public bool HasLineOfSight(IReadOnlyMap map, HexCoord from, HexCoord to) => HasLineOfSightResult;
+    }
+}
